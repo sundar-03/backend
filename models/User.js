65e227c496451ac9dbe11fc89b00c765c418eb14
Hank-learner <jerryrocker6@gmail.com>
2020-11-10 17:18:27 +0530

    removed eslint errors and formatted

--- models/User.js
+++ models/User.js
@@ -1,40 +1,34 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose'); 
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+	name: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	college: {
+		type: String,
+		required: true,
+	},
+	department: {
+		type: String,
+		required: true,
+	},
+	mobile: {
+		type: Number,
+	},
+});
 
-    },
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+userSchema.plugin(passportLocalMongoose);
 
-    },
-    college:{
-        type:String,
-        required:true,
-
-    },
-    department:{
-        type:String,
-        required:true,
-
-    },
-    mobile:{
-        type:Number,
-
-    }
-    
-})
-
-userSchema.plugin(passportLocalMongoose); 
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
